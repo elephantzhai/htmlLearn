96b992e216e2971823885f11ea95f2719fcae501
elephantzhai <elephantzhai@gmail.com>
2014-05-20 22:56:50 +0800

    nodejs2 class8 part1

---
+++ nodejs2/class 8/util/inherits.js
@@ -0,0 +1,28 @@
+var util = require("util")
+function Base(){
+	this.name = 'base'
+	this.base = 2012
+	this.sayHello = function(){
+		console.log("hello"+this.name+" this year is "+this.base)
+	}
+}
+
+Base.prototype.showName=function(){
+	console.log(this.name)
+}
+
+function Sub(){
+	this.name='sub'
+}
+util.inherits(Sub,Base)
+
+//原有输出
+var objBase = new Base()
+objBase.showName()
+objBase.sayHello()
+console.log(objBase)
+
+//继承
+var objSub = new Sub()
+objSub.showName()
+console.log(objSub)
