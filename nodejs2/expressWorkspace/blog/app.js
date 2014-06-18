b6d3edce4105dc02ea6b196002e07a4d2147fd17
elephantzhai <elephantzhai@gmail.com>
2014-06-18 23:11:52 +0800

    nodejs2 class19

--- nodejs2/expressWorkspace/blog/app.js
+++ nodejs2/expressWorkspace/blog/app.js
@@ -5,6 +5,10 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 var partials = require("express-partials")
+var session = require("express-session")
+var settings = require('./settings')
+var connect=require("connect")
+var MongoStore = require('connect-mongo')(connect)
 
 
 var routes = require('./routes/index');
@@ -22,6 +26,34 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded());
 app.use(cookieParser());
+app.use(session({
+    secret:settings.cookieSecret,
+    store:new MongoStore({
+        db:settings.db
+    })
+}))
+//使用中间件来返回成功和失败的信息
+app.use(function(req, res, next){
+    //声明变量
+    var err = req.session.error
+        , msg = req.session.success;
+    //删除会话中原有属性
+    delete req.session.error;
+    delete req.session.success;
+    //将错误和正确信息存放到动态试图助手变量中。
+    res.locals.message = '';
+    if (err) res.locals.message = '<div class="alert alert-error">' + err + '</div>';
+    if (msg) res.locals.message = '<div class="alert alert-success">' + msg + '</div>';
+    next();
+});
+//使用中间件把user设置成动态视图助手
+app.use(function(req, res, next){
+//    res.locals({
+//        user:req.session.user
+//    })
+    res.locals.user = req.session.user
+    next();
+})
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
--- nodejs2/expressWorkspace/blog/routes/index.js
+++ nodejs2/expressWorkspace/blog/routes/index.js
@@ -1,6 +1,10 @@
 var express = require('express');
 var router = express.Router();
 
+var crypto=require("crypto");
+var User=require("../models/User");
+var Post=require("../models/post")
+
 /* GET home page. */
 //root
 router.get('/', function(req, res) {
@@ -15,23 +19,74 @@ router.post('/post', function(req, res) {
 });
 //用户注册
 router.get('/reg', function(req, res) {
-    res.render('reg', { title: 'reg' });
+    res.render('reg', { title: '注册页面' });
 });
 //
-router.post('/doReg', function(req, res) {
-
+router.post('/Reg', function(req, res) {
+    //检验用户两次输入的口令是否一致
+    if(req.body["password-repeat"]!=req.body['password']){
+        req.session.error="两次输入的口令不一致";
+        return res.redirect("/reg");
+    }
+    //生成口令的散列值，我们使用md5加密
+    var md5=crypto.createHash('md5');
+    var password=md5.update(req.body.password).digest("base64");
+    //声明需要添加的用户
+    var newUser=new User({
+        name:req.body.username,
+        password:password
+    });
+    User.find(newUser.name,function(err,user){
+        //如果用户已经存在
+        if(user){
+            req.session.error="该用户已经存在";
+            return res.redirect("/reg");
+        }
+        //如果不存在则添加用户
+        newUser.save(function(err){
+            if(err){
+                req.session.error=err;
+                return res.redirect("/reg");
+            }
+            req.session.user=newUser;
+            req.session.success="注册成功";
+            res.redirect("/");
+        })
+    })
 });
 //用户登录
 router.get('/login', function(req, res) {
-    res.render('login', { title: 'login' });
+    console.log("get login");
+    res.render('login', { title: '登录页面' });
+});
+
+router.post('/login', function(req, res) {
+    console.log("post dologin");
+    //将登录的密码转成md5形式
+    var md5=crypto.createHash("md5");
+    var password=md5.update(req.body.password).digest("base64");
+    //验证用户
+    User.find(req.body.username,function(err,user){
+        //首先根据用户名查询是否存在
+        if(!user){
+            req.session.error="用户不存在";
+            return res.redirect("/login");
+        }
+        //验证密码是否正确
+        if(user.password!=password){
+            req.session.error="用户密码错误";
+            return res.redirect("/login");
+        }
+        req.session.user=user;
+        req.session.success="登录成功";
+        res.redirect("/");
+    })
 });
 //用户退出
-router.post('/doLogin', function(req, res) {
-
-});
-
 router.get('/logout', function(req, res) {
-    res.render('index', { title: 'logout' });
+    req.session.user=null;
+    req.session.success="退出成功";
+    res.redirect("/");
 });
 
 module.exports = router;
